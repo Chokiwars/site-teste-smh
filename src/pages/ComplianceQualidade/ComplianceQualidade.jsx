a5c3d2343aed95cf3ebc635e24c64d881616d440
sabrinasavini <ssavinifras@gmail.com>
2025-10-30 08:54:34 -0300

    arrumando o codigo

--- src/pages/ComplianceQualidade/ComplianceQualidade.jsx
+++ src/pages/ComplianceQualidade/ComplianceQualidade.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-import { useTranslation } from "react-i18next"; // 1. IMPORTAR (Corrigido)
+import { useTranslation } from "react-i18next";
 import {
   FileText,
   ShieldAlert,
